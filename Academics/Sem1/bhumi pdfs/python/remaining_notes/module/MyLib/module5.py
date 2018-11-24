047424c84c97bc085b6ccf6b1738fa5721af66e8
BhumikaReddy Samaseni <bhumikasamaseni@gmail.com>
2018-11-24 23:32:07 +0530

    pdfs and reversed string program

---
+++ Academics/Sem1/bhumi pdfs/python/remaining_notes/module/MyLib/module5.py
@@ -0,0 +1,6 @@
+# filename: MyLib/module5.py
+
+# module5 in directory MyLib
+def foo():
+	print("hello from module5 of MyLib")
+
---
+++ Programming/Python/reversing_string.py
@@ -0,0 +1,3 @@
+name="my name is bhumika"
+rev=name[::-1]
+print(rev)
