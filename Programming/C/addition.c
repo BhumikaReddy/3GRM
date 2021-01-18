ddd6dbad74dca8f4ca1c2b53a05f63a550700661
Bhumika <bhumikasamaseni@gmail.com>
2021-01-18 12:46:12 +0530

    c files

---
+++ Programming/C/addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter integer a: ");
+	scanf("%d",&a);
+	printf("enter integer b: ");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("a+b = %d",sum);
+	return 0;
+}
---
+++ Programming/C/intger.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("enter an integer: ");
+	scanf("%d",&a);
+	printf("number entered is %d",a);
+	return 0;
+}
